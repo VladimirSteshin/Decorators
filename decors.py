b0a88ba05ee6d192c356fccbb05f321724e8b492
Steshin <@gs.pmok>
2022-07-20 15:38:14 +0300

    Init and decors

---
+++ decors.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from functools import wraps
+from os import getcwd
+
+
+def log_default(func):
+
+    @wraps(func)
+    def inner(*args, **kwargs):
+        answer = func(*args, **kwargs)
+        date = datetime.now().ctime()
+        arguments = args, kwargs
+        for_log = f'{date} was called {func.__name__} with arguments {arguments} result = {answer}\n'
+        with open('logs_for_HW_1.txt', 'a', encoding='UTF-8') as file:
+            file.write(str(for_log))
+        return answer
+
+    return inner
+
+
+def log_path(path=getcwd()):
+
+    def decor(func):
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            answer = func(*args, **kwargs)
+            date = datetime.now().ctime()
+            arguments = args, kwargs
+            where = path
+            for_log = f'{date} in {where} was called {func.__name__} with arguments {arguments} result = {answer}\n'
+            with open(path + '\logs_for_HW_2.txt', 'a', encoding='UTF-8') as file:
+                file.write(str(for_log))
+            return answer, path
+
+        return inner
+
+    return decor
